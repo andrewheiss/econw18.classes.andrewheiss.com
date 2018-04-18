8c4118a44e6913c4321d471d520d351afec22ed7
Andrew Heiss <andrewheiss@gmail.com>
2018-04-18 12:12:56 -0600

    Scale midterm 2 scores

--- lib/score-scaling.R
+++ lib/score-scaling.R
@@ -31,15 +31,40 @@ midterm_1_adjusted <- midterm_1_raw %>%
   fuzzy_left_join(grades, by = c("pct_new" = "lower", "pct_new" = "upper"),
                   match_fun = list(`>=`, `<`))
 
-plot_midterm <- midterm_1_adjusted %>% 
+plot_midterm_1 <- midterm_1_adjusted %>% 
   count(grade)
 
-midterm_grades <- ggplot(midterm_1_adjusted, aes(x = adjusted)) +
+midterm_1_grades <- ggplot(midterm_1_adjusted, aes(x = adjusted)) +
   geom_histogram(binwidth = 2) +
   theme_light()
 
-midterm_scores <- ggplot(plot_midterm, aes(x = fct_rev(grade), y = n)) + 
+midterm_1_scores <- ggplot(plot_midterm_1, aes(x = fct_rev(grade), y = n)) + 
   geom_bar(stat = "identity") +
   theme_light()
 
-midterm_grades + midterm_scores + plot_layout(ncol = 1)
+midterm_1_grades + midterm_1_scores + plot_layout(ncol = 1)
+
+
+
+midterm_2_raw <- read_csv(file.path(here(), "private", "midterm_2.csv"))
+
+midterm_2_adjusted <- midterm_2_raw %>% 
+  rename(score = `Midterm 2`) %>% 
+  mutate(pct = score / 150) %>% 
+  mutate(adjusted = score + 1.5,
+         pct_new = adjusted / 150) %>% 
+  fuzzy_left_join(grades, by = c("pct_new" = "lower", "pct_new" = "upper"),
+                  match_fun = list(`>=`, `<`))
+
+plot_midterm_2 <- midterm_2_adjusted %>% 
+  count(grade)
+
+midterm_2_grades <- ggplot(midterm_2_adjusted, aes(x = adjusted)) +
+  geom_histogram(binwidth = 2) +
+  theme_light()
+
+midterm_2_scores <- ggplot(plot_midterm_2, aes(x = fct_rev(grade), y = n)) + 
+  geom_bar(stat = "identity") +
+  theme_light()
+
+midterm_2_grades + midterm_2_scores + plot_layout(ncol = 1)
